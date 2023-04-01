83cc9e9a6fb47f387c3a1d0af2d7bbc4b7605cb7
siddhantkale <122144668+siddhantkale@users.noreply.github.com>
2023-04-01 22:07:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var i=0;
+while(i<document.querySelectorAll(".drum").length)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonInnerHTML=this.innerHTML;
+    makesound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+});
+i++;
+}
+document.addEventListener("keydown",function(event){
+ makesound(event.key);
+ buttonAnimation(event.key);
+});
+function makesound(key){
+     switch(key){
+        case "w":
+        var w=new Audio("sounds/tom-1.mp3");
+        w.play();
+        break;
+        case "a":
+            var tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play(); 
+            break;  
+        case "s":
+            var tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play(); 
+            break;
+        case "d":
+            var tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play(); 
+            break;
+        case "j":
+            var crash=new Audio("sounds/crash.mp3");
+            crash.play(); 
+            break;
+        case "k":
+            var kick=new Audio("sounds/kick.mp3");
+            kick.play(); 
+            break;
+        case "l": 
+        var snare=new Audio("sounds/snare.mp3");
+            snare.play(); 
+            break;           
+        default:
+            console.log(buttonInnerHTML);    
+   }
+
+
+}
+function buttonAnimation(currentKey){
+  document.querySelector("."+currentKey).classList.add("pressed");
+  setTimeout(function(){ 
+    document.querySelector("."+currentKey).classList.remove("pressed");
+    },100)
+}
